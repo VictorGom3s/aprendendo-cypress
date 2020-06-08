0e59212a9659ee7cc44d07526b91b664cbf150e1
Victor Sampaio <vsampaio@pm.me>
2020-06-08 16:19:33 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.sendFile("index.html", { root: __dirname });
+});
+
+app.listen("8080", () => {
+  console.log("End to End testing example app is running");
+});
---
+++ cypress/integration/index.spec.js
@@ -0,0 +1,37 @@
+describe("Tests if the app is running", () => {
+  it("Access the URL", () => {
+    cy.visit("http://localhost:8080");
+  });
+});
+
+describe("The page must contain a title", () => {
+  it("Check if there's an page title", () => {
+    cy.get("title").should("contain", "End to End");
+  });
+
+  it("Check if there's an H1 element", () => {
+    cy.get("h1").should("contain", "Hello Cypress");
+  });
+});
+
+describe("Submit the form with valid values", () => {
+  it("Check if there are the correct inputs", () => {
+    cy.get("#name").should("be.empty");
+    cy.get("#email").should("be.empty");
+  });
+
+  it("Should have a submit button", () => {
+    cy.get("button").should("be.visible");
+  });
+
+  it("On form submit, the values must be listed in the table", () => {
+    cy.get("#name").type("Victor");
+    cy.get("#email").type("vsampaio@pm.me");
+
+    cy.get("button").click();
+
+    cy.get("tbody")
+      .should("contain", "Victor")
+      .should("contain", "vsampaio@pm.me");
+  });
+});
